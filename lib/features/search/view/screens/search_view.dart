c4d3ff47640278b84f2045e4642c8117f3c6cba0
ZiadHamadaElmorsy <ziadelshoky1@gmail.com>
2024-01-31 11:01:34 +0200

    zzzz

--- lib/features/search/view/screens/search_view.dart
+++ lib/features/search/view/screens/search_view.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appointment/features/search/view/screens/my_App_Bar.dart';
 import 'package:doctor_appointment/features/search/view/screens/tabs.dart';
 import 'package:doctor_appointment/features/search/view/screens/text_field.dart';
 import 'package:flutter/material.dart';
@@ -10,34 +11,7 @@ class SearchView extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-            leading: InkWell(
-              child: Container(
-                height: 29,
-                width: 29,
-                decoration: ShapeDecoration(
-                    image: DecorationImage(
-                        image: AssetImage(
-                            'lib/features/search/view/assets/Appbar.png'),
-                        fit: BoxFit.cover),
-                    shape: OvalBorder()),
-              ),
-              onTap: () {},
-            ),
-            title: Text(
-              'MUHIRIS',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18.55),
-            ),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 25.76),
-                child: IconButton(
-                    icon: Icon(Icons.shopping_cart), onPressed: () {}),
-              )
-            ]),
+        appBar: MyAppBar(),
         body: Padding(
           padding: const EdgeInsets.only(right: 44, top: 30, left: 30),
           child: Column(
