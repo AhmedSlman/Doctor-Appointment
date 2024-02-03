6e659f0b4a8cb9efeede9743a4cd8cf5df8d316b
AhmedSlman <ahmedslman151@gmail.com>
2024-02-03 19:10:13 +0200

    a

--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -1,4 +1,5 @@
 class AppAssets {
   static String profileImage = 'dgfgfgfdg';
   static String doctorImage = 'assets/images/doctor image .png';
+  static String logoImage = 'assets/images/Logo.png';
 }
